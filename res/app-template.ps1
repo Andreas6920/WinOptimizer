7db0d29d8eb2317304f2dad4cbaf6c2a005f41d4
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-05-28 00:39:24 +0200

    Update app-template.ps1

--- res/app-template.ps1
+++ res/app-template.ps1
@@ -1,8 +1,8 @@
 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name BurntToast -Force
-
+$Logo = "https://i.ibb.co/j82ZkYK/Microsoft.png"
 
 if (!(Test-Path "$($env:ProgramData)\chocolatey\choco.exe")) { 
-New-BurntToastNotification -Text "Windows", 'Preparing installer..'
+New-BurntToastNotification -Applogo $Logo -Text "Windows", 'Preparing installer..'
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))}
