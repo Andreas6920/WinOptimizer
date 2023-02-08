6280ac5699e04eee3193ec13c1179b70ffe870ba
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-02-08 23:36:22 +0100

    Update Start-WinAppInstall.psm1

--- modules/Start-WinAppInstall.psm1
+++ modules/Start-WinAppInstall.psm1
@@ -1,52 +1,111 @@
-﻿function Start-WinAppInstall {
-    <#param ( [Parameter(Mandatory=$true)]
+﻿function Invoke-AppInstall {
+    param ( [Parameter(Mandatory=$true)]
             [string]$Name,
             [Parameter(Mandatory=$true)]
-            [string]$App),
-            [Parameter(Mandatory=$false)]
-            [switch]$IncludeOffice)#>
-
-        param (
-            [Parameter(Mandatory=$true)]
-            [string]$Name,
-            [Parameter(Mandatory=$true)]
-            [string]$App,
-            [Parameter(Mandatory=$false)]
-            [switch]$IncludeOffice)
-
+            [string]$App)
 
     If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main")) {New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
     Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize"  -Value 1
     
+    $code = "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+    $installationfile = "$($env:ProgramData)\Winoptimizer\Invoke-AppInstall.ps1"
 
-    $chocodir = [Environment]::GetFolderPath("CommonApplicationData")
-    $chocodir = Join-Path $chocodir -ChildPath "Chocolatey"
-    If (!(Test-Path $chocodir)) {
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    if(!(test-path $installationfile)){new-item -ItemType Directory ($installationfile | Split-Path) -ea ignore | out-null; New-item $installationfile -ea ignore | out-null;}
+    if(!((get-content $installationfile) -notmatch "https://community.chocolatey.org/install.ps1")){Set-content -Encoding UTF8 -Value $code -Path $installationfile}
 
-    # Windows notification
-    Add-Type -AssemblyName System.Windows.Forms
-    $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
-    $path = (Get-Process -id $pid).Path
-    $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
-    $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
-    $balmsg.BalloonTipText = 'Installing ' + $Name 
-    $balmsg.BalloonTipTitle = "Winoptimizer"
-    $balmsg.Visible = $true
-    $balmsg.ShowBalloonTip(20000)
+    Add-content -Encoding UTF8 -Value (invoke-webrequest "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/res/app-template.ps1").Content.replace('REPLACE-ME-NAME', $Name).replace('REPLACE-ME-APP', $App) -Path $installationfile}
 
-    # Install
-    choco install $app -y | Out-Null
+    Write-Host "`tDesktop Applications:" -f Green;""; 
 
-
-
-
-    if($IncludeOffice){
+    Write-Host "`t`tBROWSER:" -f Yellow
+    Write-Host "`t`t`tChrome        Mozilla Firefox   Opera" -f Green
+    Write-Host "`t`t`tBrave         Librewolf         Vevaldi" -f Green
+    "";
+    Write-Host "`t`tTOOLS:" -f Yellow
+    Write-Host "`t`t`tDropbox       Google Drive      Teamviewer" -f Green
+    Write-Host "`t`t`t7-zip         Winrar            Greenshot" -f Green
+    Write-Host "`t`t`tShareX        Gimp              Adobe Acrobat Reader" -f Green
+    "";
+    Write-Host "`t`tMEDIA PLAYER:" -f Yellow
+    Write-Host "`t`t`tSpotify       VLC               Itunes" -f Green
+    Write-Host "`t`t`tWinamp        Foobar2000        K-Lite" -f Green
+    Write-Host "`t`t`tMPC-HC        Popcorntime" -f Green
+    "";
+    Write-Host "`t`tDevelopment:" -f Yellow
+    Write-Host "`t`t`tNotepad++       vscode          atom" -f Green
+    Write-Host "`t`t`tVim             Eclipse         PowerShell-Core " -f Green
+    Write-Host "`t`t`tPuTTY           Superputty      TeraTerm" -f Green
+    Write-Host "`t`t`tFilezilla       WinSCP          mRemoteNG" -f Green
+    Write-Host "`t`t`tWireshark       git             Windows Terminal" -f Green
+    "";
+    Write-Host "`t`tSocial:" -f Yellow
+    Write-Host "`t`t`tWebex           Zoom            Microsoft Teams" -f Green
+    Write-Host "`t`t`tDiscord         Twitch          Ubisoft-Connect" -f Green
+    "";
+    Write-Host "    ** List multiple programs seperated by , (comma) - spaces are allowed." -f Yellow;
+    "";
+    Write-Host "Type the programs you would like to be installed on this system" -nonewline;
     
-        Write-Host "`tMicrosoft office:" -f Green -nonewline;
-        
+
+    $requested_apps = (Read-Host " ").Split(",") | Foreach-object { $_ -replace ' ',''}
+    foreach ($requested_app in $requested_apps) {
+        if("cancel" -eq "$requested_app"){Write-Output "Skipping this section.."}
+        # Browsers
+            elseif("Firefox" -match "$requested_app"){Invoke-AppInstall -Name "Mozilla Firefox" -App "firefox"} 
+            elseif("Chrome" -match "$requested_app"){Invoke-AppInstall -Name "Google Chrome" -App "googlechrome"} 
+            elseif("Brave" -match "$requested_app"){Invoke-AppInstall -Name "Brave Browser" -App "brave"} 
+            elseif("Opera" -match "$requested_app"){Invoke-AppInstall -Name "Opera" -App "opera"} 
+            elseif("Vivaldi" -match "$requested_app"){Invoke-AppInstall -Name "Libre wolf" -App "librewolf"} 
+
+        # Tools
+            elseif("Dropbox" -match "$requested_app"){Invoke-AppInstall -Name "Dropbox" -App "dropbox"} 
+            elseif("Google Drive" -match "$requested_app"){Invoke-AppInstall -Name "Google Drive" -App "googledrive"} 
+            elseif("TeamViewer" -match "$requested_app"){Invoke-AppInstall -Name "TeamViewer" -App "teamviewer"} 
+            elseif("7-zip" -match "$requested_app"){Invoke-AppInstall -Name "7-Zip" -App "7Zip"} 
+            elseif("winrar" -match "$requested_app"){Invoke-AppInstall -Name "Winrar" -App "winrar"} 
+            elseif("Greenshot" -match "$requested_app"){Invoke-AppInstall -Name "Greenshot" -App "greenshot"} 
+            elseif("ShareX" -match "$requested_app"){Invoke-AppInstall -Name "ShareX" -App "sharex"} 
+            elseif("Gimp" -match "$requested_app"){Invoke-AppInstall -Name "Gimp" -App "gimp"} 
+            elseif("Adobe" -match "$requested_app"){Invoke-AppInstall -Name "Adobe Acrobat Reader" -App "adobereader"} 
+        # Media Player
+            elseif("spotify" -match "$requested_app"){Invoke-AppInstall -Name "Spotify" -App "Spotify"}  
+            elseif("VLC" -match "$requested_app"){Invoke-AppInstall -Name "VLC" -App "VLC"}  
+            elseif("itunes" -match "$requested_app"){Invoke-AppInstall -Name "iTunes" -App "itunes"}  
+            elseif("Winamp" -match "$requested_app"){Invoke-AppInstall -Name "Winamp" -App "Winamp"}  
+            elseif("foobar2000" -match "$requested_app"){Invoke-AppInstall -Name "foobar2000" -App "foobar2000"}  
+            elseif("K-lite" -match "$requested_app"){Invoke-AppInstall -Name "K-lite" -App "k-litecodecpackfull"}  
+            elseif("MPC-HC" -match "$requested_app"){Invoke-AppInstall -Name "MPC-HC" -App "MPC-HC"}  
+            elseif("popcorn" -match "$requested_app"){Invoke-AppInstall -Name "Popcorntime" -App "popcorntime"}  
+        # Development
+            elseif("notepad++" -match "$requested_app"){Invoke-AppInstall -Name "Notepad++" -App "notepadplusplus"}  
+            elseif("vscode" -match "$requested_app"){Invoke-AppInstall -Name "Visual Studio Code" -App "vscode"}  
+            elseif("atom" -match "$requested_app"){Invoke-AppInstall -Name "atom" -App "atom"}  
+            elseif("vim" -match "$requested_app"){Invoke-AppInstall -Name "vim" -App "vim"} 
+            elseif("Eclipse" -match "$requested_app"){Invoke-AppInstall -Name "Eclipse" -App "Eclipse"} 
+            elseif("putty" -match "$requested_app"){Invoke-AppInstall -Name "PuTTY" -App "putty"} 
+            elseif("superputty" -match "$requested_app"){Invoke-AppInstall -Name "SuperPutty" -App "superputty"} 
+            elseif("teraterm" -match "$requested_app"){Invoke-AppInstall -Name "Tera Term" -App "teraterm"} 
+            elseif("Filezilla" -match "$requested_app"){Invoke-AppInstall -Name "Filezilla" -App "filezilla"} 
+            elseif("WinSCP" -match "$requested_app"){Invoke-AppInstall -Name "WinSCP" -App "WinSCP"} 
+            elseif("mremoteng" -match "$requested_app"){Invoke-AppInstall -Name "MremoteNG" -App "mremoteng"} 
+            elseif("wireshark" -match "$requested_app"){Invoke-AppInstall -Name "Wireshark" -App "wireshark"} 
+            elseif("git" -match "$requested_app"){Invoke-AppInstall -Name "git" -App "git"}
+            elseif("Powershell-Core" -match "$requested_app"){Invoke-AppInstall -Name "PowerShell Core" -App "powershell-core"}
+            elseif("Windows terminal" -match "$requested_app"){Invoke-AppInstall -Name "Windows terminal" -App "microsoft-windows-terminal"}
+        # Social
+            elseif("Microsoft Teams" -match "$requested_app"){Invoke-AppInstall -Name "Microsoft Teams" -App "microsoft-teams"} 
+            elseif("Zoom" -match "$requested_app"){Invoke-AppInstall -Name "Zoom" -App "zoom"} 
+            elseif("Webex" -match "$requested_app"){Invoke-AppInstall -Name "Webex" -App "webex"}
+            elseif("Twitch" -match "$requested_app"){Invoke-AppInstall -Name "Twitch" -App "twitch"}
+            elseif("Ubisoft Connect" -match "$requested_app"){Invoke-AppInstall -Name "Ubisoft Connect" -App "ubisoft-connect"}
+    }
+
+    DO {
+        Write-Host "`tWould you like to Install Microsoft Office? (y/n)" -f Green -nonewline;
+        $answer1 = Read-host " " 
+            Switch ($answer1) { 
+      
+            y {        
       
                 # Choose version
                     "";
@@ -60,9 +119,9 @@
                         Write-Host "`t`tWhich version would you prefer?" -f Green -nonewline;
                         $answer2 = Read-host " "
                         if("$answer2" -eq "Cancel"){Write-Host "`tSkipping this section.."}                         
-                        elseif("$answer2" -match "365")       {$ver = "O365BusinessRetail"; $officename = "Microsoft 365";}
-                        elseif("$answer2" -match "2019")      {$ver = "HomeBusiness2019Retail"; $officename = "Microsoft Office 2019";}
-                        elseif("$answer2" -match "2016")      {$ver = "HomeBusinessRetail"; $officename = "Microsoft Office 2016"}}
+                        elseif("$answer2" -match "365")       {$ver = "O365BusinessRetail"; $name = "Microsoft 365";}
+                        elseif("$answer2" -match "2019")      {$ver = "HomeBusiness2019Retail"; $name = "Microsoft Office 2019";}
+                        elseif("$answer2" -match "2016")      {$ver = "HomeBusinessRetail"; $name = "Microsoft Office 2016"}}
                     While($ver -notin "O365BusinessRetail", "HomeBusiness2019Retail","HomeBusinessRetail")     
               
                 # Choose Language
@@ -93,40 +152,29 @@
                         elseif("$answer3" -match "^Rus")   {$lang = "ru-ru"}
                         elseif("$answer3" -match "^Swe")   {$lang = "sv-se"}}
                       While($lang -notin "en-us", "de-de","es-es","da-dk","fr-fr","ja-jp","nb-no","ru-ru","sv-se")
-
-                    # Windows notification 1
-                        Add-Type -AssemblyName System.Windows.Forms
-                        $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
-                        $path = (Get-Process -id $pid).Path
-                        $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
-                        $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
-                        $balmsg.BalloonTipText = 'Installing ' + $officename + '...'
-                        $balmsg.BalloonTipTitle = "Microsoft Office"
-                        $balmsg.Visible = $true
-                        $balmsg.ShowBalloonTip(20000)
-                    
-                    # Installation
-                        Get-AppxPackage | Where-Object Name -Match "Microsoft.MicrosoftOfficeHub|Microsoft.Office.OneNote" | Remove-AppxPackage;
-                        choco install microsoft-office-deployment /64bit /Product $ver /language REPLACE-ME-LANGUAGE -y
-                        
-                    
-                    # Windows notification 2
-                        Add-Type -AssemblyName System.Windows.Forms
-                        $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
-                        $path = (Get-Process -id $pid).Path
-                        $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
-                        $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
-                        $balmsg.BalloonTipText = 'Installation complete!'
-                        $balmsg.BalloonTipTitle = "Microsoft Office"
-                        $balmsg.Visible = $true
-                        $balmsg.ShowBalloonTip(20000)    
-                        
-                        }
-
-
+                  
+                #Installation
+                    # Modify install script
+                        # Download
+                            $link = "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/res/install-office.ps1"
+                            $installationfile = "$($env:ProgramData)\Winoptimizer\Invoke-AppInstall.ps1"
+                            if(!(test-path $installationfile)){new-item -ItemType Directory ($installationfile | Split-Path) -ea ignore | out-null; New-item $installationfile -ea ignore | out-null;}
+                            Add-content -Encoding UTF8 -Value (invoke-webrequest $link).Content.replace('REPLACE-ME-FULLNAME', $Name).replace('REPLACE-ME-VERSION', $ver).replace('REPLACE-ME-LANGUAGE', $lang) -Path $installationfile
+                  }
+               
+            n {Write-Host "`t`t- NO. Skipping this step."}}}
+    
+        While ($answer1 -notin "y", "n")
+    
 # Start app installation              
     Start-Process Powershell -argument "-Ep bypass -Windowstyle hidden -file `"""$($env:ProgramData)\Winoptimizer\Invoke-AppInstall.ps1""`""
 
+
+    Do {
+        Write-Host "`tWould you like to install auto-updater? (y/n)" -f Green -nonewline;
+        $answer = Read-Host " " 
+        Switch ($answer) { 
+            Y {   
                     #create update file
                     Write-Host "`t`t- Downloading updating script." -f Yellow
                     $filepath = "$env:ProgramData\chocolatey\app-updater.ps1"
@@ -135,11 +183,19 @@
                     # Create scheduled job
                     Write-Host "`t`t- scheduling update routine." -f Yellow
                     $name = 'winoptimizer-app-Updater'
-                    $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-nop -W hidden -noni -ep bypass -file $filepath"
+                    $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-W hidden -ep bypass -file $filepath"
                     $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM"-LogonType ServiceAccount -RunLevel Highest
-                    $trigger= New-ScheduledTaskTrigger -At 12:05 -Daily
+                    $trigger= New-ScheduledTaskTrigger -At 12:00 -Daily
                     $settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -DontStopIfGoingOnBatteries -RunOnlyIfIdle -DontStopOnIdleEnd -IdleDuration 00:05:00 -IdleWaitTimeout 03:00:00
 
                     Register-ScheduledTask -TaskName $Name -Taskpath "\Microsoft\Windows\Winoptimizer\" -Settings $settings -Principal $principal -Action $action -Trigger $trigger -Force | Out-Null
+                                                                        
+            }
+            N { Write-Host "`t`t- NO. Skipping this step." -f Red }}} 
+    While ($answer -notin "y", "n")
 
-}}
+    
+
+#End of function
+Write-Host "`tApp installer completed. Enjoy your freshly installed applications." -f Green
+Start-Sleep 10
