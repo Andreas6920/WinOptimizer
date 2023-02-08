8be0fda04fe7d448c03d6fd05dd6b86808b3b994
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-02-08 22:45:59 +0100

    Update Start-WinOptimizer.psm1

--- modules/Start-WinOptimizer.psm1
+++ modules/Start-WinOptimizer.psm1
@@ -1,50 +1,40 @@
-﻿# Nuget
-$packageProviders = Get-PackageProvider | Select-Object name
-if(!($packageProviders.name -contains "nuget")){Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force -Scope CurrentUser | Out-Null}
-if($packageProviders -contains "nuget"){Import-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force -Scope CurrentUser | Out-Null}
+﻿# Preparing services
+    # Nuget
+        $ProgressPreference = "SilentlyContinue" # hide progressbar
+        $packageProviders = Get-PackageProvider | Select-Object name
+        if(!($packageProviders.name -contains "nuget")){Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force -Scope CurrentUser | Out-Null}
+        if($packageProviders -contains "nuget"){Import-PackageProvider -Name NuGet -RequiredVersion 2.8.5.208 -Force -Scope CurrentUser | Out-Null}
+        $ProgressPreference = "Continue" #unhide progressbar
+    # TLS upgrade
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    # Disable Explorer first run
+        If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main")) {
+        New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
+        Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize"  -Value 1
 
-# TLS upgrade
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# Install modules
+    $modulepath = $env:PSmodulepath.split(";")[1]
 
-# Disable Explorer first run
-If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main")) {
-New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
-Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize"  -Value 1
+    $modules = @(
+        "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-WinAntiBloat.psm1"
+        "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-WinAntiHack.psm1"
+        "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-WinAppInstall.psm1"
+        "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-WinOptimizer.psm1"
+        "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Add-Reg.psm1"
+        "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Restart-Explorer.psm1"
+        "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-Input.psm1"
+        "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Stop-Input.psm1")
 
-# install modules
-#$modulepath = $env:PSmodulepath.split(";")[1]
-$modulepath = $($env:PROGRAMFILES)+"\PowerShell\Modules"
+    Foreach ($module in $modules) {
+        # prepare folder
+            $file = (split-path $module -Leaf)
+            $filename = $file.Replace(".psm1","").Replace(".ps1","").Replace(".psd","")
+            $filedestination = "$modulepath/$filename/$file"
+            $filesubfolder = split-path $filedestination -Parent
+            If (!(Test-Path $filesubfolder )) {New-Item -ItemType Directory -Path $filesubfolder -Force | Out-Null; Start-Sleep -S 1}
+        # Download module
+            (New-Object net.webclient).Downloadfile($module, $filedestination)
+        # Install module
+            if (Get-Module -ListAvailable -Name $file){ Import-module -name $filename }}
 
-$modules = @(
-    "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-WinAntiBloat.psm1"
-    "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-WinAntiHack.psm1"
-    "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-WinAppInstall.psm1"
-    "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-WinOptimizer.psm1"
-    "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Add-Reg.psm1"
-    "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Restart-Explorer.psm1"
-    "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Start-input.psm1"
-    "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/modules/Stop-input.psm1"    
-)   
 
-foreach ($module in $modules) {
-
-# prepare folder
-$file = (split-path $module -Leaf)
-$filename = $file.Replace(".psm1","").Replace(".ps1","").Replace(".psd","")
-$filedestination = "$modulepath/$filename/$file"
-$filesubfolder = split-path $filedestination -Parent
-If (!(Test-Path $filesubfolder )) {New-Item -ItemType Directory -Path $filesubfolder -Force | Out-Null; Start-Sleep -S 1}
-
-# Download module
-write-host "`t - installing module: $file"
-(New-Object net.webclient).Downloadfile($module, $filedestination)
-#Invoke-WebRequest -Uri $modulepath -OutFile $filedestination; Start-Sleep -s 1
-
-# Install module
-#Import-module -name $filename
-
-}
-
-# (get-module | where name -eq Winoptimizer2).ExportedCommands
-
-Get-Module | Get-Member -MemberType Property | Format-Table Name  
