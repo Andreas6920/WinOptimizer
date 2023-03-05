5d5f829600adbb87a511f26c50db6440e0ba99c7
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-03-05 23:16:13 +0100

    Update app-updater.ps1

--- scripts/app-updater.ps1
+++ scripts/app-updater.ps1
@@ -1,29 +1,23 @@
-# Updater
+# Get latest version of script
     if(Test-Connection www.github.com -Quiet){
     $link = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/scripts/app-updater.ps1").Content
     $scriptlocation = Join-Path -path $env:programdata -ChildPath "\Chocolatey\app-updater.ps1"
     $scriptcontent = Get-content $scriptlocation
     if (!($link -eq $scriptcontent )){set-content -Value $link -Path $scriptlocation -Force; set-location ($scriptlocation| Split-Path -Parent); .\app-updater.ps1}}
 
-$date = (get-date -f "yyyy/MM/dd - HH:mm:ss")
-$check_updates = choco outdated
-$log = "$env:ProgramData\chocolatey\app-updater_log.txt"
+# Updater script
+    $date = (get-date -f "yyyy/MM/dd - HH:mm:ss")
+    $check_updates = choco outdated
+    $log = "$env:ProgramData\chocolatey\app-updater_log.txt"
 
-# if no updates available, just write to logs that scan is done and nothing found.
-if ($check_updates -match "Chocolatey has determined 0 package") 
-{
-echo "`n$date - No update(s) found :)`n" $check_updates >> $log
-echo "`n###################################################################################################" >> $log
-}
+    # if no updates available, just write to logs that scan is done and nothing found.
+    if ($check_updates -match "Chocolatey has determined 0 package") 
+    {   echo "`n$date - No update(s) found :)`n" $check_updates >> $log
+        echo "`n###################################################################################################" >> $log}
 
-# if updates found, update and add to logs
-else
-{
-echo "`n$date - UPDATE(S) FOUND!! :O`n$" $check_updates >> $log
-Checkpoint-Computer -Description "winoptimizer - appupdater" -RestorePointType "APPLICATION_INSTALL" | Out-Null #out-null waits for complete
-choco upgrade all -y >> $log
-echo "`n###################################################################################################" >> $log
-}
-
-
-# test 123
+    # if updates found, update and add to logs
+    else
+    {   echo "`n$date - UPDATE(S) FOUND!! :O`n$" $check_updates >> $log
+        Checkpoint-Computer -Description "winoptimizer - appupdater" -RestorePointType "APPLICATION_INSTALL" | Out-Null #out-null waits for complete
+        choco upgrade all -y >> $log
+        echo "`n###################################################################################################" >> $log}
