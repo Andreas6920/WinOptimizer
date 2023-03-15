8516c4f9a3e22eab9ae3916f20d2aca86061cd7a
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-03-16 00:53:39 +0100

    Update app-updater.ps1

--- scripts/app-updater.ps1
+++ scripts/app-updater.ps1
@@ -1,12 +1,15 @@
 # Prepare varibles
-    $version = "Version 2.8"
+    $version = "Version 2.9"
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
     $link = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/scripts/app-updater.ps1").Content
     $scriptlocation = Join-Path -path $env:programdata -ChildPath "\Chocolatey\app-updater.ps1"
-    $date = (get-date -f "yyyy/MM/dd - HH:mm:ss")
     $check_updates = choco outdated
+    $check_updates_negative = choco outdated | select-string "Chocolatey has determined"
+    $update = choco upgrade all -y
     $loglocation = Join-path -Path $env:ProgramData -ChildPath "\chocolatey\app-updater_log.txt"
 
-#  Wait for inactivity
+#  Wait for inactivity    
     do {$a = [System.Windows.Forms.Cursor]::Position
         Start-Sleep -s 240
         $b = [System.Windows.Forms.Cursor]::Position} 
@@ -16,15 +19,18 @@
     if(Test-Connection www.github.com -Quiet){
     if (!($link -cMatch $version )){write-host "updating..."; start-sleep -s 3; set-content -Value $link -Path $scriptlocation -Force; set-location ($scriptlocation| Split-Path -Parent)}}
 
-# Updater script
-    # if no updates available, just write to logs that scan is done and nothing found.
-    if ($check_updates -match "Chocolatey has determined 0 package") 
-    {   echo "`n$date - No update(s) found :)`n" $check_updates >> $loglocation
-        echo "`n###################################################################################################" >> $loglocation}
+# if updates not found, add that to the logs.
+    if ($check_updates -match "Chocolatey has determined 0 package"){
+        $date = get-date -f "yyyy/MM/dd - HH:mm:ss"
+        Add-Content -Value "`n$date - No update(s) found :)`n" -Path $loglocation -Encoding UTF8
+        Add-Content -Value $check_updates_negative -Path $loglocation -Encoding UTF8
+        Add-Content -Value "`n###################################################################################################" -Path $loglocation -Encoding UTF8}
 
-    # if updates found, update and add to logs
-    else
-    {   echo "`n$date - UPDATE(S) FOUND!! :O`n$" $check_updates >> $loglocation
-        Checkpoint-Computer -Description "winoptimizer - appupdater" -RestorePointType "APPLICATION_INSTALL" | Out-Null #out-null waits for complete
-        choco upgrade all -y >> $loglocation
-        echo "`n###################################################################################################" >> $loglocation}
+# if updates found, update and add to logs
+    else{
+        $date = get-date -f "yyyy/MM/dd - HH:mm:ss"
+        Add-Content -Value "`n$date - UPDATE(S) FOUND!! :O`n" -Path $loglocation -Encoding UTF8
+        Add-Content -Value $check_updates -Path $loglocation -Encoding UTF8
+        Checkpoint-Computer -Description "Winoptimizer - appupdater" -RestorePointType "APPLICATION_INSTALL" | Out-Null
+        Add-Content -Value $update -Path $loglocation -Encoding UTF8
+        Add-Content -Value "`n###################################################################################################" -Path $loglocation -Encoding UTF8}
