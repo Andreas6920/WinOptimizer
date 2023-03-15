343952521c1aa1084d141e5351727ba1bb9b86b5
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-03-15 22:21:20 +0100

    Update app-updater.ps1

--- scripts/app-updater.ps1
+++ scripts/app-updater.ps1
@@ -12,13 +12,11 @@
         $b = [System.Windows.Forms.Cursor]::Position} 
     while ($b -ne $a)
 
-
 # Get latest version of script
     if(Test-Connection www.github.com -Quiet){
     if (!($link -cMatch $version )){write-host "updating..."; start-sleep -s 3; set-content -Value $link -Path $scriptlocation -Force; set-location ($scriptlocation| Split-Path -Parent)}}
 
 # Updater script
-
     # if no updates available, just write to logs that scan is done and nothing found.
     if ($check_updates -match "Chocolatey has determined 0 package") 
     {   echo "`n$date - No update(s) found :)`n" $check_updates >> $loglocation
