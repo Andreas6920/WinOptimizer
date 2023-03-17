05ae2ecb5f8c3fef8ec9bbda8e30f119f5778a7b
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-03-17 14:57:30 +0100

    Update app-updater.ps1

--- scripts/app-updater.ps1
+++ scripts/app-updater.ps1
@@ -1,5 +1,5 @@
-# Prepare varibles
-    $version = "Version 3.0"
+# Prepare variables
+    $version = "Version 3.1"
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
     [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
     $link = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/scripts/app-updater.ps1").Content
@@ -7,7 +7,6 @@
     $check_updates = choco outdated
     $check_updates_negative = choco outdated | select-string "Chocolatey has determined"
     $check_updates_positive = choco outdated | select-string "false"
-    $update = choco upgrade all -y | Select-string "has been installed."
     $loglocation = Join-path -Path $env:ProgramData -ChildPath "\chocolatey\app-updater_log.txt"
 
 #  Wait for inactivity    
@@ -29,9 +28,10 @@
 
 # if updates found, update and add to logs
     else{
+        $update = choco upgrade all -y | Select-string "has been installed."
         $date = get-date -f "yyyy/MM/dd - HH:mm:ss"
-        Add-Content -Value "`n$date - UPDATE(S) FOUND!! :O`n" -Path $loglocation -Encoding UTF8
-        Add-Content -Value $check_updates_positive -Path $loglocation -Encoding UTF8
+        Add-Content -Value "`n$date - OUTDATED APPLICATIONS DETECTED!!`n" -Path $loglocation -Encoding UTF8
+        Add-Content -Value $check_updates_positive.Replace('|false','') -Path $loglocation -Encoding UTF8
         Checkpoint-Computer -Description "Winoptimizer - appupdater" -RestorePointType "APPLICATION_INSTALL" | Out-Null
-        Add-Content -Value $update -Path $loglocation -Encoding UTF8
+        Add-Content -Value $update.Replace('installed','updated') -Path $loglocation -Encoding UTF8
         Add-Content -Value "`n###################################################################################################" -Path $loglocation -Encoding UTF8}
