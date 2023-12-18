b755b408ab6aa76ecd0a1f150ebdd4741c65cce0
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-12-18 14:06:49 +0100

    version 3.0 release

--- modules/Restart-Explorer.psm1
+++
@@ -1,8 +0,0 @@
-﻿Function restart-explorer{
-    <# When explorer restarts with the regular stop-process function, the active PowerShell loses focus,
-     which means you'll have to click on the window in order to enter your input. here's the hotfix. #>
-    taskkill /IM explorer.exe /F | Out-Null -ErrorAction SilentlyContinue
-    start explorer | Out-Null
-    $windowname = $Host.UI.RawUI.WindowTitle
-    Add-Type -AssemblyName Microsoft.VisualBasic
-    [Microsoft.VisualBasic.Interaction]::AppActivate($windowname)}
