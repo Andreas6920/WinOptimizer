f293035008a3b277fbcaa40eca5b02da0c1efe56
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2023-12-18 14:39:46 +0100

    Update Winoptimizer.ps1

--- Winoptimizer.ps1
+++ Winoptimizer.ps1
@@ -2,20 +2,20 @@
     # TLS upgrade
     Clear-Host
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
+    Write-host "Loading" -NoNewline
     # Disable Explorer first run
         $RegistryKey = "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main"
             If (!(Test-Path $RegistryKey)) {New-Item -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Force | Out-Null}
-            if(!(Get-Item "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\" | ? Property -EQ "DisableFirstRunCustomize")){Write-host "`t- Disable First Run Internet Explorer.."; Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 1}
+            if(!(Get-Item "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main\" | ? Property -EQ "DisableFirstRunCustomize")){Write-host "." -NoNewline; Set-ItemProperty -Path  "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 1}
     
     # Nuget Installation
         if(!(test-path "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\2.8.5.208")){
-            $ProgressPreference = "SilentlyContinue"; Start-Sleep -S 1; Write-host "`t- Install Nuget";  
+            $ProgressPreference = "SilentlyContinue"; Start-Sleep -S 1; Write-host "." -NoNewline;  
             Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null}
     
     # Create Base Folder
         $BaseFolder = Join-path -Path ([Environment]::GetFolderPath("CommonApplicationData")) -Childpath "WinOptimizer"
-        if(!(test-path $BaseFolder)){mkdir $BaseFolder -ErrorAction SilentlyContinue | Out-Null }
+        if(!(test-path $BaseFolder)){Write-host "." -NoNewline; mkdir $BaseFolder -ErrorAction SilentlyContinue | Out-Null }
 
     # Preparing Scripts
         $scripts = @(
@@ -25,6 +25,7 @@
         Foreach ($script in $scripts) {
 
         # Download Scripts
+            Write-host "." -NoNewline;
             $filename = split-path $script -Leaf
             $filedestination = join-path $BaseFolder -Childpath $filename
             (New-Object net.webclient).Downloadfile("$script", "$filedestination")
