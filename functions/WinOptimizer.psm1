07ce102a94e7d78ef82ec3ec86c0be7dd9f4e566
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2024-01-03 00:32:10 +0100

    Update WinOptimizer.psm1

--- functions/WinOptimizer.psm1
+++ functions/WinOptimizer.psm1
@@ -1,8 +1,12 @@
-Function Restart-Explorer{
+Function Restart-Explorer {
             <# When explorer restarts with the regular stop-process function, the active PowerShell loses focus,
             which means you'll have to click on the window in order to enter your input. here's the hotfix. #>
-            taskkill /IM explorer.exe /F | Out-Null -ErrorAction SilentlyContinue
-            start explorer | Out-Null
+            if(Get-Process -Name Explorer){
+            Stop-Process -Name "Explorer" -Force
+            Start-Sleep -Seconds 2
+            if(!(Get-Process -Name Explorer)){Start-Process Explorer}}
+            #taskkill /IM explorer.exe /F | Out-Null -ErrorAction SilentlyContinue
+            #start explorer | Out-Null
             $windowname = $Host.UI.RawUI.WindowTitle
             Add-Type -AssemblyName Microsoft.VisualBasic
             [Microsoft.VisualBasic.Interaction]::AppActivate($windowname)}
