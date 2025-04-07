b278258191e0fb5073f6166f9ec7f535d6049639
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-04-07 13:59:02 +0200

    Update Winoptimizer.ps1

--- Winoptimizer.ps1
+++ Winoptimizer.ps1
@@ -17,28 +17,26 @@
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Installér modul
-$ModuleUrl = "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/functions/WinOptimizer.psm1"
-$ModuleName = [System.IO.Path]::GetFileNameWithoutExtension((Split-Path $ModuleUrl -Leaf))
-$ModulePath = $env:PSModulePath.Split(";")[1]
-$ModuleFolder = Join-Path -Path $ModulePath -ChildPath $ModuleName
-$ModuleLocation = Join-Path -Path $Modulefolder -ChildPath (Split-Path $ModuleUrl -Leaf)
+    $ModuleUrl = "https://raw.githubusercontent.com/Andreas6920/WinOptimizer/main/functions/WinOptimizer.psm1"
+    $ModuleName = [System.IO.Path]::GetFileNameWithoutExtension((Split-Path $ModuleUrl -Leaf))
+    $ModulePath = $env:PSModulePath.Split(";")[1]
+    $ModuleFolder = Join-Path -Path $ModulePath -ChildPath $ModuleName
+    $ModuleLocation = Join-Path -Path $Modulefolder -ChildPath (Split-Path $ModuleUrl -Leaf)
 
     # Opretter modulmappen, hvis den ikke eksisterer
-    if (-not (Test-Path $ModuleFolder)) {
-        New-Item -ItemType Directory -Path $ModuleFolder -Force | Out-Null}
+        if (-not (Test-Path $ModuleFolder)) { New-Item -ItemType Directory -Path $ModuleFolder -Force | Out-Null}
 
     # Download modulet
-    Write-Host "$(Get-LogDate)`t    - Henter modulet til $ModulePath" -ForegroundColor Green
-    (New-Object Net.WebClient).DownloadFile($ModuleUrl, $ModuleLocation)
+        Write-Host "$(Get-LogDate)`t    - Henter modulet til $ModulePath" -ForegroundColor Green
+        (New-Object Net.WebClient).DownloadFile($ModuleUrl, $ModuleLocation)
     
     # Kontroller, om filen er blevet downloadet korrekt
-    if (Test-Path $ModuleLocation) {Write-Host "$(Get-LogDate)`t    - Module downloaded successfully." -ForegroundColor Green
+        if (Test-Path $ModuleLocation) {Write-Host "$(Get-LogDate)`t    - Module downloaded successfully." -ForegroundColor Green
 
         # Installer og importer modulet
-        try {
-            Import-Module -Name $ModuleName -Force -ErrorAction Stop
-            Write-Host "$(Get-LogDate)`t    - Modulet hentet." -ForegroundColor Green}
-        catch {
-            Write-Host "$(Get-LogDate)`t    - Failed to import module: $_" -ForegroundColor Red}} 
-
-    else {  Write-Host "$(Get-LogDate)`t    - Module download failed." -ForegroundColor Red }
+            try {
+                Import-Module -Name $ModuleName -Force -ErrorAction Stop
+                Write-Host "$(Get-LogDate)`t    - Modulet hentet." -ForegroundColor Green}
+            catch {
+                Write-Host "$(Get-LogDate)`t    - Failed to import module: $_" -ForegroundColor Red}} 
+        else {  Write-Host "$(Get-LogDate)`t    - Module download failed." -ForegroundColor Red }
