4ba289b84876f54d256fad2f62d97efbe7684118
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-04-09 12:39:03 +0200

    Update Start-WinSettings.ps1

--- scripts/Start-WinSettings.ps1
+++ scripts/Start-WinSettings.ps1
@@ -3,11 +3,13 @@ Function Start-WinSettings {
         [switch]$EnableDarkMode
     )
     
+# System Preparation
+
     # Ensure admin rights
-	If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
-		# Relaunch as an elevated process
-		$Script = $MyInvocation.MyCommand.Path
-		Start-Process powershell.exe -Verb RunAs -ArgumentList "-ExecutionPolicy RemoteSigned", "-File `"$Script`""}
+        If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+            # Relaunch as an elevated process
+            $Script = $MyInvocation.MyCommand.Path
+            Start-Process powershell.exe -Verb RunAs -ArgumentList "-ExecutionPolicy RemoteSigned", "-File `"$Script`""}
 
     # Tjek om systemet er Windows 11 baseret eller 10
         $BuildNumber = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").CurrentBuildNumber
@@ -44,26 +46,38 @@ Write-Host "$(Get-LogDate)`t    Configure Windows:" -f Green
         Write-Host "$(Get-LogDate)`t        - Disabling bing search results in windows search menu" -f Yellow
         Add-Reg -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type "DWORD" -Value "0"
         Start-Sleep -S 2
+    
+    # Disable automatic setup of network connected devices
+        Write-Host "$(Get-LogDate)`t        - Turn off automatic setup of network connected devices." -f Yellow
+        Add-Reg -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\NcdAutoSetup\Private" -Name "AutoSetup" -Type "DWORD" -Value "0"
+        Start-Sleep -s 2
 
-        if($ThisIsWindows10){
-            # Taskbar: Hide task view button
-                Write-Host "$(Get-LogDate)`t        - Taskbar: Hide task view" -f Yellow
-                Add-Reg -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MultiTaskingView" -Name "ShowTaskViewButton" -Type "DWORD" -Value "0"
-                Start-Sleep -S 2
+    # Disable sharing of PC and printers
+        Write-Host "$(Get-LogDate)`t        - Turn off sharing of PC and Printers." -f Yellow
+        Get-NetConnectionProfile | ForEach-Object {Set-NetConnectionProfile -Name $_.Name -NetworkCategory Public -ErrorAction SilentlyContinue | Out-Null}    
+        get-printer | Where-Object shared -eq True | ForEach-Object {Set-Printer -Name $_.Name -Shared $False -ErrorAction SilentlyContinue | Out-Null}
+        netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=No -ErrorAction SilentlyContinue | Out-Null
 
-            # Remove 3D objects
-                Write-Host "$(Get-LogDate)`t        - Disabling 3D Objects app" -f Yellow
-                $3Dlocation32bit = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}"
-                $3Dlocation64bit = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A"
-                If((test-path $3Dlocation32bit )){remove-item $3Dlocation32bit}
-                If((test-path $3Dlocation64bit )){remove-item $3Dlocation64bit}
-                Start-Sleep -S 2}
 
-        if($ThisIsWindows11){
-            # Move Taskbar to the left side
-            Write-Host "$(Get-LogDate)`t        - Move taskbar icons to left" -f Yellow
-            Add-Reg -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Type "DWord" -Value "0"
+    if($ThisIsWindows10){
+        # Taskbar: Hide task view button
+            Write-Host "$(Get-LogDate)`t        - Taskbar: Hide task view" -f Yellow
+            Add-Reg -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\MultiTaskingView" -Name "ShowTaskViewButton" -Type "DWORD" -Value "0"
+            Start-Sleep -S 2
+
+        # Remove 3D objects
+            Write-Host "$(Get-LogDate)`t        - Disabling 3D Objects app" -f Yellow
+            $3Dlocation32bit = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}"
+            $3Dlocation64bit = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A"
+            If((test-path $3Dlocation32bit )){remove-item $3Dlocation32bit}
+            If((test-path $3Dlocation64bit )){remove-item $3Dlocation64bit}
             Start-Sleep -S 2}
+
+    if($ThisIsWindows11){
+        # Move Taskbar to the left side
+        Write-Host "$(Get-LogDate)`t        - Move taskbar icons to left" -f Yellow
+        Add-Reg -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Type "DWord" -Value "0"
+        Start-Sleep -S 2}
 
     if($EnableDarkMode){    
         Write-Host "$(Get-LogDate)`t        - Enabling Dark Mode" -f Yellow
